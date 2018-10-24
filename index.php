ad5d4e12e7f69428b41099b60ea38bd567d6e223
unknown <alelelene@gmail.com>
2018-10-24 23:45:59 +0800

    Initial Commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>La Crisse Catering Services</title>
+    <link rel="stylesheet" href="./public/css/style.css">
+  </head>
+  <body>
+    <div class="banner">
+    </div>
+  </body>
+</html>
